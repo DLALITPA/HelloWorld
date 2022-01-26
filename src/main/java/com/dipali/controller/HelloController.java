3fea2fecbbfac61fe36304feefd93f40bb0ccc6a
Lalit Patil <dipali.lalit-patil@capgemini.com>
2022-01-26 13:55:25 +0530

    HelloWorld Committed

---
+++ src/main/java/com/dipali/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.dipali.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String hello()
+	{
+		System.out.println("HELLO WORLD DIPALI!");
+		return "HELLO WORLD DIPALI!!";
+	}
+}
+
